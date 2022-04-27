c84914da03bfb3c855bd577dc45af4653cfa8ea7
JiwonDev <sylvester7412@gmail.com>
2022-04-27 23:56:01 +0900

    코드 리뷰 반영

--- src/main/java/com/samsung/StringCal.java
+++ src/main/java/com/samsung/StringCal.java
@@ -2,20 +2,26 @@ package com.samsung;
 
 public final class StringCal {
 
-    int add(String str) {
+    public int add(String str) {
         if (str.isEmpty()) {
             return 0;
         }
 
         if (str.contains("+")) {
             String[] nums = str.split("\\+");
-
-            int sum = 0;
-            for (int i = 0; i < nums.length; i++) {
-                sum += Integer.parseInt(nums[i]);
-            }
-            return sum;
+            return sum(nums);
+        } else {
+            return 0;
         }
-        return Integer.parseInt(str);
     }
+
+    public int sum(String[] nums) {
+        int result = 0;
+        for (int i = 0; i < nums.length; i++) {
+            result += Integer.parseInt(nums[i]);
+        }
+        return result;
+    }
+
+
 }
