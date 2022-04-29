252f4db8bc7da437a5fe50ed8e0ea61dc1acdd44
somyiha <104494633+somyiha@users.noreply.github.com>
2022-04-29 16:31:12 +0900

    문자열 계산기 사칙연산

--- src/main/java/com/samsung/Main.java
+++ src/main/java/com/samsung/Main.java
@@ -7,13 +7,19 @@ public final class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("덧셈식을 입력해 주세요.");
-
+//        System.out.println("덧셈식을 입력해 주세요.");
+        System.out.println("계산식을 입력해 주세요");
 
         String str = scanner.nextLine();
         StringCal stringCal = new StringCal();
 
-        System.out.println(stringCal.add(str));
+//        System.out.println(stringCal.add(str));
+        System.out.println(stringCal.calculate(str));
+
+//        StringCal stringCalculator = new StringCal();
+//
+//        String[] a = stringCalculator.split("3+4-5*6/2");
+//        System.out.println(stringCalculator.calString(a));
     }
 
 }
