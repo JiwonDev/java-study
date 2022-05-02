bbf08d7e2d6a2536f91bf55186bab633d6b00030
Eco_Min <guldor10.b@gmail.com>
2022-05-02 23:13:28 +0900

    Conflict error solve

--- src/main/java/com/_min_StringCalculator/domain/Calculator.java
+++ src/main/java/com/_min_StringCalculator/domain/Calculator.java
@@ -7,14 +7,21 @@ import java.util.Map;
 public class Calculator {
 
     private final Map<String, Operator> operatorMap;
+<<<<<<< HEAD
     private final double UNEXPECTED_EXPRESSION = 0D;
+=======
+>>>>>>> 82595c3 (Strign Cal)
 
     public Calculator() {
         this.operatorMap = Map.of(
                 "+", (a, b) -> a + b,
                 "-", (a, b) -> a - b,
                 "/", (a, b) -> {
+<<<<<<< HEAD
                     if(b == UNEXPECTED_EXPRESSION){
+=======
+                    if(b == 0D){
+>>>>>>> 82595c3 (Strign Cal)
                         throw new RuntimeException("0으로 나누기 불가");
                     }
                     return a / b;
@@ -31,15 +38,23 @@ public class Calculator {
             List<String> operators = preParedCalculator.getOperators(fun);
             return calculation(numbers, operators);
         }
+<<<<<<< HEAD
 
         return UNEXPECTED_EXPRESSION;
+=======
+        return 0D;
+>>>>>>> 82595c3 (Strign Cal)
     }
 
     private Double calculation(List<Double> numbers, List<String> operators) {
         Iterator<String> iterator = operators.iterator();
         return numbers.stream().reduce((x, y) ->
                 operatorMap.get(iterator.next()).apply(x, y))
+<<<<<<< HEAD
                 .orElseGet(() -> UNEXPECTED_EXPRESSION);
+=======
+                .orElseGet(() -> 0D);
+>>>>>>> 82595c3 (Strign Cal)
     }
 
 
